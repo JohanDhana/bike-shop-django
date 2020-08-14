e073267e7c3e8801853155e4a50e658a36b7290b
Andrit <kurtiandrit@hotmail.com>
2020-08-14 17:02:46 +0200

    bike details page
    some fixes on layout part

--- shop/views.py
+++ shop/views.py
@@ -10,3 +10,10 @@ def index(request):
     BMX_bikes = Bikes.objects.filter(bike_categories="BMX")
     context = {'kid_bikes': kid_bikes, 'BMX_bikes': BMX_bikes}
     return render(request, 'shop/index.html', context)
+
+
+def details(request, name):
+    selected_bike = Bikes.objects.get(name=name)
+    show_bike = {'bike': selected_bike}
+    return render(request, 'shop/details.html', show_bike)
+
