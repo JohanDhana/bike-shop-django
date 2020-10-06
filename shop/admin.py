bdd7dd823579a3891a95c751626231655cc7acb6
Andrit <kurtiandrit@hotmail.com>
2020-10-06 17:54:39 +0200

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-
 # Register your models here.
 from .models import Bikes
-admin.site.register(Bikes)
+
+admin.site.site_header = 'Bike shop'
+
+
+class BikesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bike_categories')
+    list_filter = ('size', 'bike_categories')
+
+
+admin.site.register(Bikes, BikesAdmin)
+
