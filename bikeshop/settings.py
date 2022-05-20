dd838c52f5de7b978479441207df8a3c5b7ef358
Andrit <kurtiandrit@hotmail.com>
2022-05-20 10:20:34 +0200

    new config

--- bikeshop/settings.py
+++ bikeshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f=&$ishz26(3)!(7lt5roc^s&68!wedu7(1v=wn5z3i8h%@r*y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bike-shop-django.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
