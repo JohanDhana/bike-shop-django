c7defc4ff14c8ebd749a94f8ff4056df2244559b
Johan Dhana <48352084+JohanDhana@users.noreply.github.com>
2025-05-14 10:40:05 +0200

    update to django 5

--- bikeshop/settings.py
+++ bikeshop/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import  django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -141,4 +140,3 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-django_heroku.settings(locals())
